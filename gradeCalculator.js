3f436ff8974f85c5d3771651c62e1ad5f7460e18
hoboJF <jadenfiero@gmail.com>
2025-01-06 18:14:13 -0500

    added logic to make sure that final weight isnt above 100 and also added calculation for required final exam grade

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -6,6 +6,11 @@ const calculate = () => {
     targetGrade = parseFloat(targetGrade);
     finalWeight = parseFloat(finalWeight);
     if (isNaN(currentGrade) || isNaN(targetGrade) || isNaN(finalWeight)){
-        alert("One or more inputs are invalid, please retry!");
+        alert("One or more inputs are invalid, please try again!");
     }
+    if(finalWeight > 100){
+        alert("Final weight can't be above 100%!");
+    }
+    finalGrade = (targetGrade - (currentGrade * (100 - finalWeight) / 100)) / (finalWeight / 100);
+    alert(finalGrade);
 }
