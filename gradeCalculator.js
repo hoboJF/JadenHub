a7d27e0b790210bd2093e9d869803b3c260653c0
hoboJF <jadenfiero@gmail.com>
2025-01-07 10:31:13 -0500

    added a popup that appears with invalid inputs or the final grade calculation instead of using alerts

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -6,13 +6,22 @@ const calculate = () => {
     targetGrade = parseFloat(targetGrade);
     finalWeight = parseFloat(finalWeight);
     if (isNaN(currentGrade) || isNaN(targetGrade) || isNaN(finalWeight)){
-        alert("One or more inputs are invalid, please try again!");
+        showPopup("One or more inputs are invalid, please try again!");
         return;
     }
     if(finalWeight > 100){
-        alert("Final weight can't be above 100%!");
+        showPopup("Final weight can't be above 100%!");
         return;
     }
     finalGrade = (targetGrade - (currentGrade * (100 - finalWeight) / 100)) / (finalWeight / 100);
-    alert(finalGrade);
+    showPopup(`You need a ${finalGrade.toFixed(2)}% on the final.`);
 }
+const showPopup = (message) => {
+    const popup = document.getElementById("popup");
+    const popupMessage = document.getElementById("popupMessage");
+    popupMessage.textContent = message;
+    popup.style.display = "flex";
+}
+const closePopup = () => {
+    document.getElementById("popup").style.display = "none";
+};
